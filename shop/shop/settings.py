f5649c237ed8a80f32ccdb6c3e3f520e97c07f77
MPIGA-ODOUMBA JESSE <86076146+mpigajesse@users.noreply.github.com>
2025-05-04 21:01:22 +0100

    update user

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -108,7 +108,7 @@ elif os.environ.get('POSTGRES_USER') and os.environ.get('POSTGRES_PASSWORD'):
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('POSTGRES_DB', 'mpjfums_bd'),  # Nom personnalisé par défaut
-            'USER': os.environ.get('POSTGRES_USER', 'admin'),     # Utilisateur personnalisé par défaut
+            'USER': os.environ.get('POSTGRES_USER', 'postgres'),     # Utilisateur personnalisé par défaut
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
             'HOST': os.environ.get('PGHOST', 'localhost'),
             'PORT': os.environ.get('PGPORT', '5432'),
