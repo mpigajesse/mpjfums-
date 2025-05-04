c5d0d466c9fb0862109dac2bafb5ee1ff09e0150
MPIGA-ODOUMBA JESSE <86076146+mpigajesse@users.noreply.github.com>
2025-05-04 17:05:00 +0100

    Augmentation de la longueur max du champ nom à 150 caractères

--- shop/parfums/models.py
+++ shop/parfums/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Categorie(models.Model):
-    nom = models.CharField(max_length=100)
+    nom = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     
     def __str__(self):
         return self.nom
 
 class Parfum(models.Model):
-    nom = models.CharField(max_length=100)
+    nom = models.CharField(max_length=150)
     description = models.TextField()
     prix = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
