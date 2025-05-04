8cb135f2a131ad6e65841f0d216b460b1dcc41fc
MPIGA-ODOUMBA JESSE <86076146+mpigajesse@users.noreply.github.com>
2025-05-04 16:04:01 +0100

    git commit -m "Configuration pour déploiement Railway"

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d7kd8rj$0*ps41$3ny+%r5p^szqofj7@*mmc$=hsyfbhm#&_x7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
